71bd914dce84d2307f4d8472fc2cabcd7fdbcc9e
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2021-06-08 20:20:29 -0300

    Update
    Finishing the showImgs.js, where  I consume the datas via fetch in the URL Github

--- js/functions/showImgs.js
+++ js/functions/showImgs.js
@@ -1,71 +1,24 @@
 const worksContainer = document.querySelector('#showWorks');
 
-const srcWorks = [
-    {
-        authors: ["Luiz Guilherme", "Maria Fernanda", "Maria Julia", "Stefanny"],
-        work: "./assets/img/works/work1.png",
-    },
-    {
-        authors: ["Indefinido"],
-        work: "./assets/img/works/work2.png",
-    },
-    {
-        authors: ["Diogo Augusto"],
-        work: "./assets/img/works/work3.png",
-    },
-    {
-        authors: ["Clube de Jogos"],
-        work: "./assets/img/works/work4.png",
-    },
-    {
-        authors: ["Maria Eduarda Faria", "Maria Eduarda Domingos"],
-        work: "./assets/img/works/work5.png",
-    },
-    {
-        authors: ["Clube de Jogos"],
-        work: "./assets/img/works/work6.png",
-    },
-    {
-        authors: ["Indefinido"],
-        work: "./assets/img/works/work7.png",
-    },
-    {
-        authors: ["Luciana"],
-        work: "./assets/img/works/work8.png",
-    },
-    {
-        authors: ["Victor Miguel", "Sebastyan Spira"],
-        work: "./assets/img/works/work9.png",
-    },
-    {
-        authors: ["Maria Eduarda Barone"],
-        work: "./assets/img/works/work10.png",
-    },
-    {
-        authors: ["Arielle Siqueira"],
-        work: "./assets/img/works/work11.png",
-    },
-    {
-        authors: ["Luana"],
-        work: "./assets/img/works/work12.png",
-    },
-];
-
-srcWorks.forEach((works) => {
-    worksContainer.innerHTML += `
-        <figure>
-            <img class="works__draw" src="${works.work}" alt="Trabalho de " />
-            <figcaption class="content">
-                ${
-                    works.authors == "Indefinido"
-                    ? ''
-                    : `
-                        <h2 class="subtitle">Integrantes:</h2><!--subtitle-->
-                        <p>${works.authors}</p>
-                    `
-                }
-            </figcaption>
-        </figure><!--works__draw-->
-        <hr class="container bar--black"><!--bar-->
-    `;
+fetch('https://raw.githubusercontent.com/GabrielWolf-Dev/Nb_school/main/api/db.json')
+.then(response => response.json())
+.then(data => {
+    data.forEach((works) => {
+        worksContainer.innerHTML += `
+            <figure>
+                <img class="works__draw" src="${works.work}" alt="Trabalho de " />
+                <figcaption class="content">
+                    ${
+                        works.authors == "Indefinido"
+                        ? ''
+                        : `
+                            <h2 class="subtitle">Integrantes:</h2><!--subtitle-->
+                            <p>${works.authors}</p>
+                        `
+                    }
+                </figcaption>
+            </figure><!--works__draw-->
+            <hr class="container bar--black"><!--bar-->
+        `;
+    });
 });
